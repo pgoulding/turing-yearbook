f27bafc2796468f26ef3f41e270bb347fb50dddc
Patrick Goulding <patrickgoulding@gmail.com>
2019-06-24 18:49:25 -0600

    add JSX to diplay each person with their info

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Person.css';
+import PropTypes from "prop-types";
 
 // PERSON COMPONENT CODE GOES HERE
+class Person extends Component {
+  render() {
+    return (
+      <div className="Instructor__card" data-id={this.props.id}>
+        <img src={this.props.image} alt={this.props.name}></img>
+        <h3>{this.props.name}</h3>
+        <p className="User__quote">{this.props.quote}</p>
+        <p>{this.props.superlative}</p>
+      </div>
+    )
+  }
+}
 
 export default Person;
