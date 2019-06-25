05041e52c167110cfd542cb9fe3a4fe7b29a7a91
Patrick Goulding <patrickgoulding@gmail.com>
2019-06-24 18:48:19 -0600

    add class for cohort and a method to iterate through and make each person

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person';
 import './Cohort.css';
 
 // COHORT COMPONENT CODE GOES HERE
+class Cohort extends Component {
+  render () {
+    return this.props.staffData.map((person)=> (
+      <Person key={person.id} person={person.name} image={person.photo} quote={person.quote} superlative={person.superlative}/>
+    ))
+  }
+}
 
 export default Cohort;
